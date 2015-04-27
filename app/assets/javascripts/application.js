a3269c7fc507458c0077912d048dfccc768060c8
Jake Marwil <rjm02006@gmail.com>
2015-04-27 17:47:27 -0600

    set up a simple angular map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery_ujs
 //= require angular
 //= require_tree .
+
+(function() {
+  angular.module('app', [])
+  .controller('MainController', MainController)
+  ;
+
+  MainController.$inject = ['$scope']; 
+  function MainController($scope) {
+    $scope.corndog = 'Corn-batter-fried meat tube!';
+  }
+})();
